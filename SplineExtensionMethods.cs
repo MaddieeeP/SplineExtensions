05cebf383dea94936d25f96f709be94a549efa8d
Maddie <maddieeeperryyy@gmail.com>
2024-01-23 19:25:19 +0000

    Removed expensive methods and added out parameters
    Removed GetForward() and GetUp(); added out parameters to GetClosestEndPoint for forward and up

--- SplineExtensionMethods.cs
+++ SplineExtensionMethods.cs
@@ -8,30 +8,12 @@ using UnityEngine.Splines;
 
 public static class SplineExtensionMethods
 {
-    public static Vector3 GetPoint(this SplineContainer splineContainer, int splineIndex, float progress, out Vector3 forward, out Vector3 upVector)
+    public static Vector3 GetPoint(this SplineContainer splineContainer, int splineIndex, float progress, out Vector3 forward, out Vector3 up)
     {
         splineContainer.Evaluate(splineIndex, progress, out float3 float3Pos, out float3 float3Forward, out float3 float3Up);
         forward = ((Vector3)float3Forward).normalized;
-        upVector = ((Vector3)float3Up).normalized;
+        up = ((Vector3)float3Up).normalized;
         return (Vector3)float3Pos;
-    }
-
-    public static Vector3 GetPoint(this SplineContainer splineContainer, int splineIndex, float progress)
-    {
-        splineContainer.Evaluate(splineIndex, progress, out float3 float3Pos, out float3 float3Forward, out float3 float3Up);
-        return (Vector3)float3Pos;
-    }
-
-    public static Vector3 GetForward(this SplineContainer splineContainer, int splineIndex, float progress)
-    {
-        splineContainer.Evaluate(splineIndex, progress, out float3 float3Pos, out float3 float3Forward, out float3 float3Up);
-        return (Vector3)float3Forward;
-    }
-
-    public static Vector3 GetUp(this SplineContainer splineContainer, int splineIndex, float progress)
-    {
-        splineContainer.Evaluate(splineIndex, progress, out float3 float3Pos, out float3 float3Forward, out float3 float3Up);
-        return (Vector3)float3Up;
     }
 
     public static Vector3 GetNormal(this SplineContainer splineContainer, int splineIndex, float progress, Vector3 attackVector)
@@ -43,6 +25,37 @@ public static class SplineExtensionMethods
         Vector3 normal = Quaternion.Euler(attackVector).DivideBy(Quaternion.Euler(-1f * Vector3.up)) * (Vector3)float3Up;
 
         return normal.normalized;
+    }
+
+    public static Vector3 GetClosestEndPoint(this SplineContainer splineContainer, Vector3 position, out float progress, out Vector3 forward, out Vector3 up, out float distance)
+    {
+        Vector3 end1 = splineContainer.GetPoint(0, 0f, out Vector3 forward1, out Vector3 up1);
+        Vector3 end2 = splineContainer.GetPoint(0, 1f, out Vector3 forward2, out Vector3 up2);
+        float dist1 = Vector3.Distance(position, end1);
+        float dist2 = Vector3.Distance(position, end2);
+        if (dist1 > dist2)
+        {
+            progress = 1f;
+            forward = forward2;
+            up = up2;
+            distance = dist2;
+            return end2;
+        }
+        progress = 0f;
+        forward = forward1;
+        up = up1;
+        distance = dist1;
+        return end1;
+    }
+
+    public static Vector3 GetClosestPoint(this SplineContainer splineContainer, Vector3 position, out float progress, out float distance, int resolution = 4, int iterations = 2)
+    {
+        Spline spline = splineContainer.Spline;
+        Vector3 translatedPosition = splineContainer.transform.InverseTransformPoint(position);
+        distance = SplineUtility.GetNearestPoint<Spline>(spline, translatedPosition.ToFloat3(), out float3 float3Pos, out progress, resolution, iterations);
+        Vector3 pointPosition = splineContainer.transform.TransformPoint((Vector3)float3Pos);
+
+        return pointPosition;
     }
 
     public static List<Vector3> GetPointPositionsWithOffsets(this SplineContainer splineContainer, int splineIndex, float progress, List<Vector3> offsets)
@@ -65,7 +78,7 @@ public static class SplineExtensionMethods
         return positions;
     }
 
-    public static List<List<Vector3>> GetVerts(this SplineContainer splineContainer, List<Vector3> offsets, int resolution, float portion = 1f)
+    public static List<List<Vector3>> GetVerts(this SplineContainer splineContainer, List<Vector3> offsets, int resolution, float portion = 1f) //FIX - rename or refactor maybe
     {
         List<List<Vector3>> verts = new List<List<Vector3>>();
 
@@ -79,32 +92,5 @@ public static class SplineExtensionMethods
         }
 
         return verts;
-    }
-
-    public static Vector3 GetClosestEndPoint(this SplineContainer splineContainer, Vector3 position, out float progress, out float distance)
-    {
-        Vector3 end1 = splineContainer.GetPoint(0, 0f);
-        Vector3 end2 = splineContainer.GetPoint(0, 1f);
-        float dist1 = Vector3.Distance(position, end1);
-        float dist2 = Vector3.Distance(position, end2);
-        if (dist1 > dist2)
-        {
-            progress = 1f;
-            distance = dist2;
-            return end2;
-        }
-        progress = 0f;
-        distance = dist1;
-        return end1;
-    }
-
-    public static Vector3 GetClosestPoint(this SplineContainer splineContainer, Vector3 position, out float progress, out float distance, int resolution = 4, int iterations = 2)
-    {
-        Spline spline = splineContainer.Spline;
-        Vector3 translatedPosition = splineContainer.transform.InverseTransformPoint(position);
-        distance = SplineUtility.GetNearestPoint<Spline>(spline, translatedPosition.ToFloat3(), out float3 float3Pos, out progress, resolution, iterations);
-        Vector3 pointPosition = splineContainer.transform.TransformPoint((Vector3)float3Pos);
-
-        return pointPosition;
     }
 }
